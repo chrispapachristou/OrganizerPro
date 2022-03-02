60834c191baf32707eb96d65cb3d4e0003016aa3
Christos Papachristou <>
2022-03-02 21:24:06 +0200

    v0.2 UI optimized

---
+++ app/src/main/java/com/archon/organizerpro/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.archon.organizerpro.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
